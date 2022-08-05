413b3004ccb8d161aa6af210b57768c004eb573f
adnan-rolustech <34885658+adnan-rolustech@users.noreply.github.com>
2022-08-05 11:07:50 +0500

    Initial Commit

---
+++ Firebase_Template_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase_Template_iOS
+//
+//  Created by Ahmad Rafiq on 8/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
